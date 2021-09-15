e54357503ea1fca812273f51abf26ea65c3a8520
unknown <nasrullah_hussaini@yahoo.com>
2021-09-15 18:06:13 -0400

    add contents to the skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,18 +5,62 @@ function Skills() {
     <div className="skills">
       <h1 className="skills-header">Skills</h1>
       <div className="skills-container">
-        <div className="skill-development">
+        <div className="skill-development-conainter">
           <h2>Development</h2>
 
-          <div className="development">
-            <p>JavaScript</p>
+          <div className="developments">
+            <div className="development">
+              <p>JavaScript</p>
+            </div>
+            <div className="development">
+              <p>React</p>
+            </div>
+            <div className="development">
+              <p>HTML</p>
+            </div>
+            <div className="development">
+              <p>NodeJs</p>
+            </div>
+            <div className="development">
+              <p>SASS</p>
+            </div>
+            <div className="development">
+              <p>Bootstrap</p>
+            </div>
+            <div className="development">
+              <p>Material UI</p>
+            </div>
+            <div className="development">
+              <p>MySQL</p>
+            </div>
+            <div className="development">
+              <p>Postgres</p>
+            </div>
+            <div className="development">
+              <p>MongoDB</p>
+            </div>
+            <div className="development">
+              <p>Java</p>
+            </div>
+            <div className="development">
+              <p>Ruby</p>
+            </div>
           </div>
-          <div className="development">
-            <p>React</p>
-          </div>
+        </div>
+        <div className="skill-tests">
+          <h2>Test</h2>
+          <p>Mocha and Chai</p>
+          <p>Jest</p>
+          <p>Cypress</p>
+          <p>Selenium</p>
         </div>
         <div className="skill-tools">
           <h2>Tools</h2>
+          <p>Git + Github</p>
+          <p>Bitbucket</p>
+          <p>Command Line</p>
+          <p>Chrome DevTools</p>
+          <p>Postman and Rested</p>
         </div>
       </div>
     </div>
