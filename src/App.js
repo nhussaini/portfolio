06d9d31c2e4aacb507ebb07a4004da5c11cc7f0d
unknown <nasrullah_hussaini@yahoo.com>
2021-09-14 23:27:09 -0400

    add react particles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
@@ -7,6 +8,26 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 1000,
+              },
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
       <Navbar />
       <Header />
     </>
