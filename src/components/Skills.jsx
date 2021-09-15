fc86ef68b9c5f41c363825b9119cd1114403caf9
unknown <nasrullah_hussaini@yahoo.com>
2021-09-15 16:13:31 -0400

    create skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <div className="skills">
+      <h1 className="skills-header">Skills</h1>
+      <div className="skills-container">
+        <div className="skill-development">
+          <h2>Development</h2>
+
+          <div className="development">
+            <p>JavaScript</p>
+          </div>
+          <div className="development">
+            <p>React</p>
+          </div>
+        </div>
+        <div className="skill-tools">
+          <h2>Tools</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
