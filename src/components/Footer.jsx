8497054adb951a25d6c4db78ece0cad8e05541bf
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-19 19:04:31 -0400

    add contents to footer comp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ const Footer =()=>{
     return(
         <div className="footer">
             <div className="container">
+                <div className="row">
                 <div className="col-lg-4 col-md-6 col-md-6">
                     <div className="d-flex">
                         <p>Toronto,ON</p>
@@ -10,6 +11,26 @@ const Footer =()=>{
                     <div className="d-flex">
                         <a href="tel:+14379854458">(+1)4379854458</a>
                     </div>
+
+                    <div className="d-flex">
+                        <p>nhussaini1994@gmail.com</p>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 cold-md-2 cold-sm-6">
+                    <div className="row">
+                        <div className="col">
+                            <div className="footer-nav">Home</div>
+                            <div className="footer-nav">About Me</div>
+                            <div className="footer-nav">Services</div>
+                        </div>
+                        <div className="col">
+                            <div className="footer-nav">Experience</div>
+                            <div className="footer-nav">Portfolio</div>
+                            <div className="footer-nav">Contacts</div>
+                        </div>
+                    </div>
+                </div>
                 </div>
 
             </div>
