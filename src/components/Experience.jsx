9fe910a804227743df89bb492ee6b8f482eba467
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-18 23:13:04 -0400

    add experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,10 @@
+const Experience =()=>{
+    return(
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5"></div>
+            <h1>experience</h1>
+        </div>
+    );
+}
+
+export default Experience;
