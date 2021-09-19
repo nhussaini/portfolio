e29f205a318c29b7535595a057e366cf3ce10e62
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-18 23:34:06 -0400

    add experience in exp comp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,63 @@
 const Experience =()=>{
     return(
         <div className="experience">
-            <div className="d-flex justify-content-center my-5"></div>
-            <h1>experience</h1>
+            <div className="d-flex justify-content-center my-5">
+                <h1>experience</h1>
+            </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, 
+                            adipisicing elit. Fugiat, eligendi delectus 
+                            quaerat modi porro eum quam rerum vel quis corporis ullam
+                            aliquid fugit sint inventore. Nemo accusantium eligendi pariatur assumenda!
+                        </p>
+                    </div>
+                </div>
+
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, 
+                            adipisicing elit. Fugiat, eligendi delectus 
+                            quaerat modi porro eum quam rerum vel quis corporis ullam
+                            aliquid fugit sint inventore. Nemo accusantium eligendi pariatur assumenda!
+                        </p>
+                    </div>
+                </div>
+
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, 
+                            adipisicing elit. Fugiat, eligendi delectus 
+                            quaerat modi porro eum quam rerum vel quis corporis ullam
+                            aliquid fugit sint inventore. Nemo accusantium eligendi pariatur assumenda!
+                        </p>
+                    </div>
+                </div>
+
+
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, 
+                            adipisicing elit. Fugiat, eligendi delectus 
+                            quaerat modi porro eum quam rerum vel quis corporis ullam
+                            aliquid fugit sint inventore. Nemo accusantium eligendi pariatur assumenda!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
