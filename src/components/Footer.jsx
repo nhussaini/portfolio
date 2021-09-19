fa8ca3c7c773f14c49bf209d3f0677b151a3da3b
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-19 13:18:42 -0400

    create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer =()=>{
+    return(
+        <div className="footer">
+            <div className="container">
+                <div className="col-lg-4 col-md-6 col-md-6">
+                    <div className="d-flex">
+                        <p>Toronto,ON</p>
+                    </div>
+
+                    <div className="d-flex">
+                        <a href="tel:+14379854458">(+1)4379854458</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
