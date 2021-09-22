4a8381ddcd7d78eba0fb9b9a51d0d171898bf478
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-22 13:58:16 -0400

    add porfolio project

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Portfolio() {
+    return (
+        <div className="portfolio-wrapper">
+            <div className="container">
+                <h1 className="text-uppercase text-center py-5">Portfolio</h1>
+                <div className="image-box--wrapper row justify-content-center">
+                    <img src="" alt="" className="portfolio-img" />
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Portfolio
