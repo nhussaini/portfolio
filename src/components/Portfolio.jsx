10e1ed8c132e36137f39d1cb6e1217f13f34baf4
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-22 17:48:30 -0400

    add popup box for all the projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -20,7 +20,9 @@ function Portfolio() {
         const content = (
             <>
                 <img className="portfolio-image-popupbox" src={essentailWords} alt="essential words ..."/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium sint eos magni quia voluptatem odit sapiente deleniti accusamus temporibus aspernatur architecto.</p>
+                <p>This app is for those who want to improve their vocabulary to be further ready to take the TOEFL test. It has two parts. A user can either use the Learn Mode,
+                   which lets learn new words, or the user can use the Quiz Mode which lets take a quiz and finally get a score.
+                </p>
                 <b>Github:</b> <a className="hyper-link" onClick={()=> window.open("https://github.com/nhussaini/essential-words-toefl")}>https://github.com/nhussaini/essential-words-toefl</a><br/>
                 <b>Live Demo:</b> <a className="hyper-link" onClick={()=> window.open("https://essential-words-for-toefl.web.app/")}>https://essential-words-for-toefl.web.app/</a>
 
@@ -34,10 +36,96 @@ function Portfolio() {
     const popupboxConfigEssentialWords ={
         titleBar: {
             enable: true,
-            text: 'Essential Words for TOEFL'
+            text: 'Essential'
         },
         fadeIn:true,
         fadeInSpeed: 500
+
+    }
+
+     //Schedular
+     const openPopupboxSchedular =()=>{
+        const content = (
+            <>
+                <img className="portfolio-image-popupbox" src={schedular} alt="Schedular..."/>
+                <p>
+                    Interview Scheduler is a Single Page Application(SPA) to track students interviews
+                    built with the latest tools and techniques for optimized user experience. The App
+                    allows users to add, edit and delete appointments in real time. It uses React built-in
+                    and custom hooks Data is persisted by the API server using a PostgreSQL database.
+                    The client application communicates with an API server over HTTP, using the JSON format.
+                    Moreover, for quality assurance, the project follows best practices of Test Driven Development (TDD)
+                    .i.e, individual Component is tested in isolation as well as End-to-End testing is performed.
+                </p>
+                <b>Github:</b> <a className="hyper-link" onClick={()=> window.open("https://github.com/nhussaini/lighthouse-labs-scheduler")}>https://github.com/nhussaini/lighthouse-labs-scheduler</a><br/>
+            </>
+        )
+      
+
+        PopupboxManager.open({content})
+    }
+
+    const popupboxConfigSchedular ={
+        titleBar: {
+            enable: true,
+            text: 'Schedular'
+        },
+        fadeIn:true,
+        fadeInSpeed: 500
+
+    }
+
+     //Trek
+     const openPopupboxTrek =()=>{
+        const content = (
+            <>
+                <img className="portfolio-image-popupbox" src={trek} alt="trek..."/>
+                <p>
+                TREK is a travel planning app inspired by the growing need to travel after months of lockdown. It allows users to explore any city they type in and shows local attractions, weather and images of the location.
+                 This app also lets a user create a plan of their trip by adding attractions and tasks to a personalized to-do list.
+                </p>
+                <b>Github:</b> <a className="hyper-link" onClick={()=> window.open("https://github.com/nhussaini/Travel_planner")}>https://github.com/nhussaini/Travel_planner</a><br/>
+            </>
+        )
+      
+
+        PopupboxManager.open({content})
+    }
+
+    const popupboxConfigTrek ={
+        titleBar: {
+            enable: true,
+            text: 'TREK'
+        },
+        fadeIn:true,
+        fadeInSpeed: 500
+
+    }
+
+
+    //Twitter
+    const openPopupboxTweeter =()=>{
+        const content = (
+            <>
+                <img className="portfolio-image-popupbox" src={tweeter} alt="trek..."/>
+                <p>
+                Tweeter is a simple, single-page Twitter clone that uses jQuery, AJAX, HTML5, SASS and CSS3. It lets the user post new Tweets. When a user submits a tweet, the list of tweets is refreshed, displaying the new tweets on the top of the list.
+                </p>
+                <b>Github:</b> <a className="hyper-link" onClick={()=> window.open("https://github.com/nhussaini/tweeter")}>https://github.com/nhussaini/tweeter</a><br/>
+            </>
+        )
+      
+
+        PopupboxManager.open({content})
+    }
+
+    const popupboxConfigTweeter = {
+        titleBar: {
+            enable: true,
+            text: 'Tweeter Clone'
+        },
+        fadeIn:true,
+        fadeInSpeed: 200
 
     }
     return (
@@ -53,7 +141,7 @@ function Portfolio() {
                 
 
                 
-                    <div className="portfolio-image-box">
+                    <div className="portfolio-image-box"onClick={openPopupboxSchedular}>
                       <img src={schedular} alt="schedular..." className="portfolio-image" />
                       <div className="overflow"></div>
                       <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
@@ -61,7 +149,7 @@ function Portfolio() {
                     </div>
                 
 
-                    <div className="portfolio-image-box">
+                    <div className="portfolio-image-box" onClick={openPopupboxTrek}>
                       <img src={trek} alt="trek..." className="portfolio-image" />
                       <div className="overflow"></div>
                       <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
@@ -69,7 +157,7 @@ function Portfolio() {
                     </div>
                 
 
-                    <div className="portfolio-image-box">
+                    <div className="portfolio-image-box" onClick={openPopupboxTweeter}>
                       <img src={tweeter} alt="tweeter..." className="portfolio-image" />
                       <div className="overflow"></div>
                       <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
@@ -79,6 +167,11 @@ function Portfolio() {
 
             </div>
             <PopupboxContainer {...popupboxConfigEssentialWords}/>
+            <PopupboxContainer {...popupboxConfigSchedular}/>
+            <PopupboxContainer {...popupboxConfigTrek}/>
+            <PopupboxContainer {...popupboxConfigTweeter}/>
+
+
             
         </div>
     )
