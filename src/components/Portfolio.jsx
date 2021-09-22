dc891d4942d15e2f0e6f5058b3dc5255c161280e
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-22 16:58:50 -0400

    create popupbox using react popup box

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,18 +4,39 @@ import schedular from '../images/schedular.png';
 import trek from '../images/trek.png';
 import tweeter from '../images/tweeter.png';
 
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+
+//react popupbox
+import {PopupboxManager,PopupboxContainer} from 'react-popupbox'
 
 
 
 function Portfolio() {
+    //essential words
+    const openPopupboxEssentialWords =()=>{
+        const content = (
+            <>
+                <img src={essentailWords} alt="essential words ..."/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium sint eos magni quia voluptatem odit sapiente deleniti accusamus temporibus aspernatur architecto.</p>
+                <b>Github:</b> <a className="hyper-link" onClick={()=> window.open("https://github.com/nhussaini/essential-words-toefl")}>https://github.com/nhussaini/essential-words-toefl</a>
+            </>
+        )
+      
+
+        PopupboxManager.open({content})
+    }
+
+    const popupboxConfigEssentialWords ={
+
+    }
     return (
         <div className="portfolio-wrapper">
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
                 <div className="image-box-wrapper">
-                    <div className="portfolio-image-box">
+                    <div className="portfolio-image-box" onClick={openPopupboxEssentialWords}>
                       <img src={essentailWords} alt="essential words ..." className="portfolio-image" />
                       <div className="overflow"></div>
                       <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
@@ -48,6 +69,7 @@ function Portfolio() {
                 </div>
 
             </div>
+            <PopupboxContainer {...popupboxConfigEssentialWords}/>
             
         </div>
     )
