e3fa9efa5ce6e1bbc87d9c993942665d8db0792f
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-22 14:43:46 -0400

    add style to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,20 +10,26 @@ function Portfolio() {
         <div className="portfolio-wrapper">
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
-                <div className="image-box--wrapper row justify-content-center">
-                    <img src={essentailWords} alt="essential words ..." className="portfolio-img" />
-                </div>
+                <div className="image-box-wrapper">
+                    <div className="portfolio-image-box">
+                      <img src={essentailWords} alt="essential words ..." className="portfolio-image" />
+                    </div>
+                
 
-                <div className="image-box--wrapper row justify-content-center">
-                    <img src={schedular} alt="schedular..." className="portfolio-img" />
-                </div>
+                
+                    <div className="portfolio-image-box">
+                      <img src={schedular} alt="schedular..." className="portfolio-image" />
+                    </div>
+                
 
-                <div className="image-box--wrapper row justify-content-center">
-                    <img src={trek} alt="trek..." className="portfolio-img" />
-                </div>
+                    <div className="portfolio-image-box">
+                      <img src={trek} alt="trek..." className="portfolio-image" />
+                    </div>
+                
 
-                <div className="image-box--wrapper row justify-content-center">
-                    <img src={tweeter} alt="tweeter..." className="portfolio-img" />
+                    <div className="portfolio-image-box">
+                      <img src={tweeter} alt="tweeter..." className="portfolio-image" />
+                    </div>
                 </div>
 
             </div>
