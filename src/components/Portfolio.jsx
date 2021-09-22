8b6f3ddcfe1be18d70156b4eab3d68d80b228d41
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-22 15:33:01 -0400

    add and style the overlay div for each project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,6 +4,10 @@ import schedular from '../images/schedular.png';
 import trek from '../images/trek.png';
 import tweeter from '../images/tweeter.png';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function Portfolio() {
     return (
@@ -13,22 +17,33 @@ function Portfolio() {
                 <div className="image-box-wrapper">
                     <div className="portfolio-image-box">
                       <img src={essentailWords} alt="essential words ..." className="portfolio-image" />
+                      <div className="overflow"></div>
+                      <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
                     </div>
                 
 
                 
                     <div className="portfolio-image-box">
                       <img src={schedular} alt="schedular..." className="portfolio-image" />
+                      <div className="overflow"></div>
+                      <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+
                     </div>
                 
 
                     <div className="portfolio-image-box">
                       <img src={trek} alt="trek..." className="portfolio-image" />
+                      <div className="overflow"></div>
+                      <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+
                     </div>
                 
 
                     <div className="portfolio-image-box">
                       <img src={tweeter} alt="tweeter..." className="portfolio-image" />
+                      <div className="overflow"></div>
+                      <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+
                     </div>
                 </div>
 
