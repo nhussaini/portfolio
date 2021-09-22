4788b5b37c2504ed3c8a0c11af3710f5e6ba344e
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-22 14:12:18 -0400

    add the images of the projects

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -38,6 +39,7 @@ function App() {
       <AboutMe />
       <Skills />
       <Experience />
+      <Portfolio />
       <Footer />
     </>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import essentailWords from '../images/essential-words.PNG';
+import schedular from '../images/schedular.png';
+import trek from '../images/trek.png';
+import tweeter from '../images/tweeter.png';
 
 
 function Portfolio() {
@@ -7,8 +11,21 @@ function Portfolio() {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
                 <div className="image-box--wrapper row justify-content-center">
-                    <img src="" alt="" className="portfolio-img" />
+                    <img src={essentailWords} alt="essential words ..." className="portfolio-img" />
                 </div>
+
+                <div className="image-box--wrapper row justify-content-center">
+                    <img src={schedular} alt="schedular..." className="portfolio-img" />
+                </div>
+
+                <div className="image-box--wrapper row justify-content-center">
+                    <img src={trek} alt="trek..." className="portfolio-img" />
+                </div>
+
+                <div className="image-box--wrapper row justify-content-center">
+                    <img src={tweeter} alt="tweeter..." className="portfolio-img" />
+                </div>
+
             </div>
             
         </div>
