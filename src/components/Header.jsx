6779417d8e47f5578881214f18950f586473af77
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-22 13:56:56 -0400

    create portfolio component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,17 @@ function Header() {
     <div id="home" className="header-wrapper">
       <div className="layover"></div>
       <div className="main-info">
-        <h1>Web development</h1>
+        <h1>Welcome 😎</h1>
         <Typed
           className="typed-text"
-          strings={["Web Design", "Web Development", "SEO"]}
+          strings={["Software Engineer", "Full-Stack Engineer", "SEO"]}
           typeSpeed={40}
           backSpeed={50}
           // attr="placeholder"
           loop
         ></Typed>
-        <a href="#" className="btn-main-offer">
-          Contact Me
+        <a href="https://resume.creddle.io/resume/cj7dillzuu9" className="btn-main-offer" target="_blank">
+          My Resume
         </a>
       </div>
     </div>
